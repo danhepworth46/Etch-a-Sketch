7141221be83edf3a5c31d507cb4e248ece558250
Dan Hepworth <93970592+danhepworth46@users.noreply.github.com>
2022-03-15 15:47:45 +0800

    Add files via upload
    3rd Commit

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,24 +1,36 @@
 let container = document.getElementById('container');
+let btn = document.querySelector('.btn');
+let gridHover = document.querySelector('.new-grid-styles');
 
-function makeRows (rows, cols){
-container.style.setProperty('--grid-rows', rows);
-container.style.setProperty('--grid-cols', cols);
-for(let c = 0; c < (rows * cols); c++){
-  let cell = document.createElement('div');
-  cell.innerText = (c + 1);
-  container.appendChild(cell).className = "grid-item";
+
+
+function makeSquares(row, col){
+  for ( let i = 0; i < (row * col); i++){
+    let item = document.createElement('div');
+    container.appendChild(item);
+    item.classList.add('item');
+    let items = document.querySelectorAll('.item');
+    items.forEach(item => item.addEventListener('mouseover', hover));
+function clearGrid(){
+    items.forEach(item => item.classList.remove('new-grid-styles'));
+    items.forEach(item => item.classList.remove('item'));
+  }
+}
+btn.addEventListener('click', clearGrid);
+}
+
+
+makeSquares(16, 16);
   
-};
-};
-makeRows(16, 16);
+function reCreateGrid(){
+    let noOfSquares = prompt("What dimensions would you like for your new Etch-a-Sketch grid?");
+    makeSquares(noOfSquares, noOfSquares);
+}
 
-let gridItem = document.querySelectorAll('.grid-item');
-console.log(gridItem)
+btn.addEventListener('click', reCreateGrid);
+
 
 function hover(){
   this.classList.add('new-grid-styles');
-
 }
-
-gridItem.forEach(cell => cell.addEventListener('mouseover', hover));
 
